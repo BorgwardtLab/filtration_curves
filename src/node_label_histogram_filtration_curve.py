8124f0aa0957b6b0d680279b571ed6f29cbee0be
lobray <leslieobray@gmail.com>
2021-07-21 12:50:07 +0200

    added docstring for node_label_histogram_curve and moved helper functions to utils

--- src/node_label_histogram_filtration_curve.py
+++ src/node_label_histogram_filtration_curve.py
@@ -34,111 +34,31 @@ from rf import *
 from preprocessing.filtration import filtration_by_edge_attribute
 
 
-
-
-def save_curves(
-        source_path="../data/labeled_datasets/BZR_MD",
-        output_path="../data/labeled_datasets/preprocessed_data/BZR_MD/"
-        ):
-    ''' Creates the filtration curves and saves each curve as a csv in
-    an output directory for easier handling in the future '''
-
-    # get all file names
-    filenames = sorted(
-        glob.glob(os.path.join(source_path, '*.pickle'))
-        )
-
-    # load all graphs
-    graphs = [
-        ig.read(filename, format='picklez') for filename in tqdm(filenames)
-    ]
-
-    # sometimes the edge weight is stored as an edge attribute; we will
-    # change this to be an edge weight
-    for graph in graphs:
-        graph.es['weight'] = [e['attribute'] for e in graph.es]
-
-    # Get all potential node labels to make sure that the distribution
-    # can be calculated correctly later on.
-    node_labels = sorted(set(
-        itertools.chain.from_iterable(graph.vs['label'] for graph in graphs)
-    ))
-
-    label_to_index = {
-        label: index for index, label in enumerate(sorted(node_labels))
-    }
-   
-    # build the filtration using the edge weights
-    filtrated_graphs = [
-        filtration_by_edge_attribute(
-            graph,
-            attribute='weight',
-            delete_nodes=True,
-            stop_early=True
-        )
-        for graph in tqdm(graphs)
-    ]
-    
-    # Create a data frame for every graph and store it; the output is
-    # determined by the input filename, albeit with a new extension.
-    for index, filtrated_graph in enumerate(tqdm(filtrated_graphs)):
-        
-        df = pd.DataFrame(columns=['graph_label, weight'].extend(node_labels))
-
-        distributions = node_label_distribution(filtrated_graph, label_to_index)
-
-        for weight, counts in distributions:
-
-            row = {
-                'graph_label': graphs[index]['label'],
-                'weight': weight
-            }
-
-            row.update({
-                str(node_label): count for node_label, count in
-                zip(node_labels, counts)
-            })
-
-            df = df.append(row, ignore_index=True)
-        
-        output_name = os.path.basename(filenames[index])
-        output_name = os.path.splitext(output_name)[0] + '.csv'
-        output_name = os.path.join(output_path, output_name)
-
-        os.makedirs(output_path, exist_ok=True)
-
-        df.to_csv(output_name, index=False)
-
-
-def load_curves(args):
-    ''' Load the stored curves '''
-    
-    # stores the graphs and graph labels
-    files = sorted(glob.glob(os.path.join(
-        "../data/labeled_datasets/preprocessed_data/" + args.dataset + "/", '*.csv'
-        )))
-
-    y = []
-    list_of_df = []
-
-    # create list of dataframes (i.e. data) and y labels
-    for idx, filename in enumerate(tqdm(files)):
-        df = pd.read_csv(filename, header=0, index_col='weight')
-        df = df.loc[~df.index.duplicated(keep="last")] 
-        y.append(df['graph_label'].values[0])
-        df = df.drop(columns='graph_label')
-        list_of_df.append(df)
-    y = LabelEncoder().fit_transform(y)
-    
-    # get the column names
-    example_file = list(pd.read_csv(files[0], header=0, index_col="weight").columns)
-    column_names = [c for c in example_file if c not in ["graph_label"]]
-
-    return list_of_df, y, column_names
-
-
 def create_curves(args):
+    '''
+    Creates the node label histogram filtration curves. 
 
+    Creates a node label histogram filtration curve, either by loading
+    the previously generated filtration curves, or by calling
+    save_curves(), which will generate the curves and same them as a csv
+    file.
+
+    Parameters
+    ----------
+    args: dict 
+        Command line arguments, used to determine the dataset 
+
+    Returns
+    -------
+    list_of_df: list
+        A list of node label filtration curves, each stored as
+        a pd.DataFrame
+    y: list
+        List of graph labels, necessary for classification.
+    column_names: list
+        List of column names (i.e. each unique node label)
+
+    '''
     # check if filtration curves are already saved. If not, generate
     # them and save them.
     if not os.path.exists("../data/labeled_datasets/" + args.dataset
--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,11 @@
 import numpy as np
 from tqdm import tqdm
 import os
+import glob
+import pandas as pd
 
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder 
 from pyper.representations import BettiCurve, make_betti_curve
 
 
@@ -110,6 +113,144 @@ def reindex_filtration_curve(filtration_curve, new_index):
             method="ffill"
             ).fillna(0)
     return filtration_curve
+
+
+def save_curves(
+        source_path="../data/labeled_datasets/BZR_MD",
+        output_path="../data/labeled_datasets/preprocessed_data/BZR_MD/"
+        ):
+    '''
+    Creates the node label filtration curves. 
+
+    Given a dataset of igraph graphs, we create a filtration curve using
+    the node label histogram. For each edge weight in the graph, we
+    generate a node label histogram for the subgraph induced by all
+    edges with weight less than or equal to that given edge weight. We
+    save each graph as a csv, so that we only do this filtration step
+    once, and can reuse the results later.
+
+    Parameters
+    ----------
+    source_path: str 
+        The path to the dataset, which are igraphs stored in a pickle
+        file.
+    output_path: str
+        The path to the output directory where the filtrations will be
+        saved as a csv (one graph per csv)
+
+    Returns
+    -------
+
+    '''
+    # get all file names
+    filenames = sorted(
+        glob.glob(os.path.join(source_path, '*.pickle'))
+        )
+
+    # load all graphs
+    graphs = [
+        ig.read(filename, format='picklez') for filename in tqdm(filenames)
+    ]
+
+    # sometimes the edge weight is stored as an edge attribute; we will
+    # change this to be an edge weight
+    for graph in graphs:
+        graph.es['weight'] = [e['attribute'] for e in graph.es]
+
+    # Get all potential node labels to make sure that the distribution
+    # can be calculated correctly later on.
+    node_labels = sorted(set(
+        itertools.chain.from_iterable(graph.vs['label'] for graph in graphs)
+    ))
+
+    label_to_index = {
+        label: index for index, label in enumerate(sorted(node_labels))
+    }
+   
+    # build the filtration using the edge weights
+    filtrated_graphs = [
+        filtration_by_edge_attribute(
+            graph,
+            attribute='weight',
+            delete_nodes=True,
+            stop_early=True
+        )
+        for graph in tqdm(graphs)
+    ]
+    
+    # Create a data frame for every graph and store it; the output is
+    # determined by the input filename, albeit with a new extension.
+    for index, filtrated_graph in enumerate(tqdm(filtrated_graphs)):
+        
+        df = pd.DataFrame(columns=['graph_label, weight'].extend(node_labels))
+
+        distributions = node_label_distribution(filtrated_graph, label_to_index)
+
+        for weight, counts in distributions:
+
+            row = {
+                'graph_label': graphs[index]['label'],
+                'weight': weight
+            }
+
+            row.update({
+                str(node_label): count for node_label, count in
+                zip(node_labels, counts)
+            })
+
+            df = df.append(row, ignore_index=True)
+        
+        output_name = os.path.basename(filenames[index])
+        output_name = os.path.splitext(output_name)[0] + '.csv'
+        output_name = os.path.join(output_path, output_name)
+
+        os.makedirs(output_path, exist_ok=True)
+
+        df.to_csv(output_name, index=False)
+
+
+def load_curves(args):
+    '''
+    Loads the precomputed node label filtration curves from csv files. 
+
+    Parameters
+    ----------
+    args: dict 
+        Command line arguments, used to determine the dataset 
+
+    Returns
+    -------
+    list_of_df: list
+        A list of node label filtration curves, each stored as
+        a pd.DataFrame
+    y: list
+        List of graph labels, necessary for classification.
+    column_names: list
+        List of column names (i.e. each unique node label)
+
+    '''
+    # stores the graphs and graph labels
+    files = sorted(glob.glob(os.path.join(
+        "../data/labeled_datasets/preprocessed_data/" + args.dataset + "/", '*.csv'
+        )))
+
+    y = []
+    list_of_df = []
+
+    # create list of dataframes (i.e. data) and y labels
+    for idx, filename in enumerate(tqdm(files)):
+        df = pd.read_csv(filename, header=0, index_col='weight')
+        df = df.loc[~df.index.duplicated(keep="last")] 
+        y.append(df['graph_label'].values[0])
+        df = df.drop(columns='graph_label')
+        list_of_df.append(df)
+    y = LabelEncoder().fit_transform(y)
+    
+    # get the column names
+    example_file = list(pd.read_csv(files[0], header=0, index_col="weight").columns)
+    column_names = [c for c in example_file if c not in ["graph_label"]]
+
+    return list_of_df, y, column_names
 
 
 def create_metric_dict(metrics=["accuracy"]):
